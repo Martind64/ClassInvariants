3588c1e3679dfc4dc9a3d34a59f1766ee7ada263
Martind64 <Martind_64@hotmail.com>
2014-12-16 12:26:19 +0100

    First commit

---
+++ ClassInvariants/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInvariants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student("Peter", 27);
+            Console.WriteLine("{0} {1}", student._name, student._age);
+
+            student._name = "";
+
+        }
+
+
+
+        
+    }
+}
---
+++ ClassInvariants/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInvariants
+{
+    public class Student
+    {
+        public string _name { get; set; }
+        public int _age { get; set; }
+
+        public Student(string name, int age)
+        {
+            Contract.Requires<ArgumentException>(!String.IsNullOrEmpty(name));
+            Contract.Requires<ArgumentException>(age >= 0);
+            _name = name;
+            _age = age;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(!string.IsNullOrEmpty(_name));
+            Contract.Invariant(_age >= 0);
+        }
+
+        
+    }
+}
